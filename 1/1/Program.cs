435d873de51059de5db21c7b98ca24a91bec627a
Deghedy <hema_2009@hotmail.com>
2018-08-01 14:54:36 +0200

    My first reporisotory

---
+++ 1/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+           //ريد كي عشان الشاشه تفضل ظاهره
+           //    عشان نخلي الكود ميتقراش "//"او نظلل ctrl+K و ctrl +c
+            //ctrl+u عشان تشيلها 
+            Console.Write("Hi");
+            Console.ReadKey();
+        }
+
+    }
+}
